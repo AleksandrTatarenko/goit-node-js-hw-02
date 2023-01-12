e9b45371b2dd161a62f76cdda8a023b48b732010
Aleksandr Tatarenko <aleksandrcrimea96@gmail.com>
2023-01-13 00:51:53 +0200

    hw

--- src/app.js
+++ src/app.js
@@ -1,36 +1,38 @@
 const express = require('express')
-const logger = require('morgan')
 const cors = require('cors')
+const logger = require('morgan')
 
-const contactsRouter = require('./routes/api/contacts')
+const contactsRouter = require('./api/api')
 
 const app = express()
+
+app.use(express.json())
+
+app.use(cors())
 
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
 app.use(logger(formatsLogger))
-app.use(cors())
-app.use(express.json())
 
-app.use('/api/contacts', contactsRouter)
+app.use('/api', contactsRouter)
 
-app.use((req, res) => {
+app.use((_, res, __) => {
   res.status(404).json({
     status: 'error',
     code: 404,
     message: 'Use api on routes: /api/contacts',
     data: 'Not found',
-  });
+  })
 })
 
-app.use((err, req, res, next) => {
-  console.log(err.stack);
+app.use((err, _, res, __) => {
+  console.log(err.stack)
   res.status(500).json({
     status: 'fail',
     code: 500,
     message: err.message,
-    data: 'Internal Server Error'
-  });
+    data: 'Internal Server Error',
+  })
 })
 
 module.exports = app
